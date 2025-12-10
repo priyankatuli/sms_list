d1d0a3244baa69c09667b335c175927494aebb75
priyankatuli <priyankaaatuli123@gmail.com>
2025-12-10 17:08:36 +0600

    new text added

--- lib/src/core/constants/app_strings.dart
+++ lib/src/core/constants/app_strings.dart
@@ -1,6 +1,6 @@
 class AppStrings{
 
-  static const String bkashTitle = 'bKash Details';
+  static const String bkashTitle = 'Transaction Details';
   //cash in
   static const String cashInTitle = 'Total Cash in\nfor this month';
   static const String cashOuTitle = 'Total Cash out\nfor this month';
